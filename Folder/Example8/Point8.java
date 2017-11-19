bdfeb04f585acee2cde444b5c08722e6d24a467e
Sai Sindhu <srajanala@users.noreply.github.com>
2017-11-18 20:46:49 -0600

    Add files via upload

---
+++ Folder/Example8/Point8.java
@@ -0,0 +1,20 @@
+package org.saisindhu.package8;
+
+public class Point8 {
+	private int x;
+	private int y;
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+
+}
